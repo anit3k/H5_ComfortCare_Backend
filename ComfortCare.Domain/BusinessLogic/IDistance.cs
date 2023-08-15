84da31cc727dbd40739c38f393932d0cfaebdc27
anit3k <steffen.halberg@gmail.com>
2023-08-15 16:48:47 +0200

    Adds baselogic for domain package

---
+++ ComfortCare.Domain/BusinessLogic/IDistance.cs
@@ -0,0 +1,10 @@
+﻿namespace ComfortCare.Domain.BusinessLogic
+{
+    /// <summary>
+    /// This interface is used by the PlanManger to get the distance and timespan on the route between address
+    /// </summary>
+    public interface IDistance
+    {
+        public (float DistanceInSeconds, float DistanceInMeters) GetDistance(string startLongitude, string startLatitude, string endLongitude, string endLatitude);
+    }
+}
